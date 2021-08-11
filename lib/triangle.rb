e69ca2d5fab981f97b1fc02b5b33ab808c9d2686
cpan1995 <cpan1995@gmail.com>
2021-08-11 17:46:04 -0400

    commit tree e4bf4cbf9e1b0aba20bbc87496fe63b1d979dfef

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,53 @@
+require "pry"
 class Triangle
   # write code here
+  attr_accessor :uniq_counter
+
+  def initialize(side1, side2, side3)
+    @uniq_counter = [side1, side2, side3]
+  end
+
+  def kind
+
+    temp_array = @uniq_counter.map(&:clone)
+    largest_num = temp_array.max
+    temp_num = temp_array.sum - temp_array.max
+
+    @uniq_counter.each { |num|
+      if num.negative?
+        begin
+          raise TriangleError
+        end
+      end
+    }
+
+    if @uniq_counter.include?(0)
+      begin
+        raise TriangleError
+      end
+    end
+    if (temp_num <= largest_num)
+      begin
+        raise TriangleError
+      end
+    end
+
+    if @uniq_counter.uniq.length == 1
+      return :equilateral
+    elsif @uniq_counter.uniq.length == 2
+      return :isosceles
+    elsif @uniq_counter.uniq.length == 3
+      return :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This isn't a triangle dude"
+    end
+  end
 end
+
+# newTriangle = Triangle.new(1, 1, 3)
+# pp newTriangle.kind
